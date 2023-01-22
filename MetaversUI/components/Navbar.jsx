5f191ecb1237a87f2c9c36a68bf3bcf5a33fb843
sanket <programmersanket@gmail.com>
2023-01-22 20:04:10 +0530

    Started metaverse project

---
+++ MetaversUI/components/Navbar.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+import {motion} from 'framer-motion';
+
+import styles from '../styles';
+import {navVariants}  from '../utils/motion';
+
+
+const Navbar = () => (
+  <motion.nav>
+
+  </motion.nav>
+);
+
+export default Navbar;
